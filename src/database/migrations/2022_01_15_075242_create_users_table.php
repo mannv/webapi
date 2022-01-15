754d456f161dd919dd70eaac9a6dca91d9b256a8
mannv <anhmantk@gmail.com>
2022-01-15 14:59:34 +0700

    create users table with migrate

---
+++ src/database/migrations/2022_01_15_075242_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('password');
+            $table->string('name');
+            $table->unsignedTinyInteger('age')->nullable();
+            $table->string('address')->nullable();
+            $table->string('tel')->nullable();
+            $table->boolean('gender')->nullable();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+}
